c4647e4a2973819532e3926fb9e2e30a7cd2030c
Shaomada <stefan.rabenstein@web.de>
2016-02-04 18:39:27 +0100

    Changed example Level for testing of the FollowPlayer update

--- Levels.elm
+++ Levels.elm
@@ -15,7 +15,17 @@ initial =
     , speedId = G.FollowMouse
     , collitionId = G.NoCollition
     }
-  , things = []
+  , things =
+    [ { angle = 0
+      , x = 300
+      , y = 0
+      , radius = 10
+      , speed = 0
+      , showId = Color.green
+      , speedId = G.FollowPlayer
+      , collitionId = G.NoCollition
+      }
+    ]
   , windowWidth = 1200
   , windowHeight = 800
   }
