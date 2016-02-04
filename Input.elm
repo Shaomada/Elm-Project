a46cb53ba6d39e804a2b1f6409db01bde4d39fd8
Shaomada <stefan.rabenstein@web.de>
2016-02-04 21:00:18 +0100

    fix for Signals not sending regularly

--- Input.elm
+++ Input.elm
@@ -58,6 +58,11 @@ input =
     Window.height
     keysParsed
   )
+  |> Signal.sampleOn
+    ( Keyboard.keysDown
+      |> Signal.map (Set.member <| Char.toCode ' ')
+      |> Time.fpsWhen 60
+    )
   |> Time.timestamp
   |> Signal.map
     ( \( t, ma ) -> 
