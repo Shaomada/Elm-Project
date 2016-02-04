b466f1db131870e638bfad9e82909fc3b655e6ce
Shaomada <stefan.rabenstein@web.de>
2016-02-04 17:52:15 +0100

    changed file to match changes from commit cad56f8

--- Shows.elm
+++ Shows.elm
@@ -1,17 +1,17 @@
 module Shows (..) where
 
-import GameTypes exposing (..)
+import GameTypes as G
 import Graphics.Collage exposing (Form)
 import Graphics.Element exposing (Element)
 
-show : Model -> Element
+show : G.Model -> Element
 show ( {player, things, windowHeight, windowWidth} as model )
   = Graphics.Collage.collage windowWidth windowHeight <|
       showThing player
       :: List.map showThing things
 
-showThing : Thing -> Form
-showThing ( {sId, x, y, radius} )
+showThing : G.Thing -> Form
+showThing ( {showId, x, y, radius} )
   =  Graphics.Collage.circle radius
-  |> Graphics.Collage.filled sId
+  |> Graphics.Collage.filled showId
   |> Graphics.Collage.move (x, y)
