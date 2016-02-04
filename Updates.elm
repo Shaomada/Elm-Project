ab3b29d4493a9cf0a74353bb1dfa65d4b9e52427
Shaomada <stefan.rabenstein@web.de>
2016-02-04 20:52:07 +0100

    implemented a touching function for collision detection

--- Updates.elm
+++ Updates.elm
@@ -59,3 +59,18 @@ updatePosition time thing =
 collition : GameModel a -> GameModel a
 collition =
     identity
+
+
+distance : Positioned a -> Positioned a -> Float
+distance th1 th2 =
+  (th1.x-th2.x)^2 + (th1.y-th2.y)^2
+
+
+minDistance : Circle a -> Circle a -> Float
+minDistance th1 th2 =
+  th1.radius + th2.radius
+
+
+touching : Positioned( Circle( a ) ) -> Positioned( Circle( a ) ) -> Bool
+touching th1 th2 =
+  distance th1 th2 <= minDistance th1 th2
