c9dea6358e5960009ac6a9748485653bc140c40a
Shaomada <stefan.rabenstein@web.de>
2016-02-05 00:48:59 +0100

    removed file which has never been used

--- Utility.elm
+++
@@ -1,7 +0,0 @@
-module Utility where
-
-import Maybe exposing (Maybe)
-import Signal exposing (Signal)
-
-mapBoth : (a -> b) -> Signal (Maybe a) -> Signal (Maybe b)
-mapBoth = Signal.map << Maybe.map
