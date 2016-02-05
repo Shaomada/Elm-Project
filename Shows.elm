0fca7da8263b6c74b35bca0ffd068906a3d465d0
Shaomada <stefan.rabenstein@web.de>
2016-02-05 11:40:28 +0100

    Now also shows the key one needs to press to set a target

--- Shows.elm
+++ Shows.elm
@@ -1,8 +1,12 @@
 module Shows (..) where
 
-import GameTypes as G
+import Text
+import String
 import Graphics.Collage exposing (Form)
 import Graphics.Element exposing (Element)
+
+import GameTypes as G
+
 
 show : G.Model -> Element
 show {things, windowHeight, windowWidth} =
@@ -14,6 +18,7 @@ showThing : G.Thing -> List Form
 showThing thing =
   [ showBody thing
   , showPath thing
+  , showKey thing
   ]
 
 
@@ -23,10 +28,22 @@ showBody thing =
   |> Graphics.Collage.filled thing.color
   |> Graphics.Collage.move (thing.x, thing.y)
 
+
 showPath : G.Thing -> Form
 showPath thing =
   case thing.movId of
     (G.MoveTowards aim) ->
       Graphics.Collage.segment (thing.x, thing.y) (aim.x, aim.y)
       |> Graphics.Collage.traced (Graphics.Collage.dotted thing.color)
+    _ -> Graphics.Collage.group []
+
+
+showKey : G.Thing -> Form
+showKey thing = case thing.inpId of
+    (G.FollowMouse c) ->
+      c
+      |> String.fromChar
+      |> Text.fromString
+      |> Graphics.Collage.text
+      |> Graphics.Collage.move (thing.x, thing.y)
     _ -> Graphics.Collage.group []
