350c922444f728d5eefae2c37363eb4bc8f04faf
Shaomada <stefan.rabenstein@web.de>
2016-02-06 14:14:04 +0100

    more Level design

--- Levels.elm
+++ Levels.elm
@@ -7,9 +7,14 @@ import Things
 initial : GameTypes.Model
 initial =
     { things =
-        [ Things.player '1' {x = 0, y = 0} 
+        [ Things.player '2' {x = 0, y = 0}
+        , Things.player '3' {x = 100, y = 0}
+        , Things.player '1' {x = -100, y = 0}
         , Things.bouncy {x = 300, y = 0}
         , Things.enemy {x = 500, y = 350}
+        , Things.enemy {x = 500, y = -350}
+        , Things.enemy {x = -500, y = 350}
+        , Things.enemy {x = -500, y = -350}
         ]
     , windowWidth = 0
     , windowHeight = 0
